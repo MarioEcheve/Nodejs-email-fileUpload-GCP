f21c38cc7182374ec4bc0ae40a8c212e43c1afab
fvilches <fvichess@outlook.com>
2020-11-23 20:05:31 -0300

    config

--- app.js
+++ app.js
@@ -155,16 +155,10 @@ app.post('/htmlToPdf', async function(request ,response){
       "format": "Letter",        // allowed units: A3, A4, A5, Legal, Letter, Tabloid     
       paginationOffset: 1,       // Override the initial pagination number
       "header": {
-        "height": "25mm",        
+        "height": "20mm",        
       },
       "footer": {
-        "height": "28mm",
-        "contents": {
-          first: 'Cover page',
-          2: 'Second page', // Any page number is working. 1-based index
-          default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
-          last: 'Last Page'
-        }
+        "height": "20mm",        
       },
     }
     const pdf = require('html-pdf');
